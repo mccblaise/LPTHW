e3427cfdbb195af7528f010c897e43125ba2af14
mccblaise <mccaffrey229@gmail.com>
2017-10-25 22:51:51 -0400

    Initial Commit On branch master  Changes to be committed: 	new file:   ex1.py 	new file:   ex2.py 	new file:   ex3.py 	new file:   ex4.py 	new file:   ex5.py 	new file:   ex6.py

---
+++ ex2.py
@@ -0,0 +1,8 @@
+#they're teaching stupid comments now...
+#hope this course isn't a waste of time
+
+print "I could have code liek this." # and comment here
+
+#they can be every where and disable code duh
+
+print "this will run..."
---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Christian B. McCaffrey'
+my_age = 22
+my_height = 74
+my_weight = 160
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print "Let's talk about %s" % my_name
+print "He's %d incehs tall." % my_height
+print "He's %d pounds." % my_weight
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
